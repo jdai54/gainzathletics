bef0f1bcc51f5c4ba5687ae488a5d3b2f72ee510
Jake <jdai54@gmail.com>
2018-07-02 15:06:56 -0700

    Added landing page view, controller, and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+  root 'welcome#index'
   # This line mounts Spree's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to
   # Spree::ProductsController.
@@ -7,6 +11,6 @@ Rails.application.routes.draw do
   #
   # We ask that you don't use the :as option here, as Spree relies on it being
   # the default of "spree".
-  mount Spree::Core::Engine, at: '/'
+  mount Spree::Core::Engine, at: '/shop'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
